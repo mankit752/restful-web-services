bd8757092f35860d08e7bd24291c5dfd605ee8c1
mankit752 <mankit752@gmail.com>
2021-07-05 17:36:04 +0530

    Basic Validations to Request

--- src/main/java/com/learning/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/learning/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.learning.rest.webservices.restfulwebservices.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +30,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date()
+                ,"Validation Failed", ex.getBindingResult().toString());
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/learning/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/learning/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = userDaoService.saveUser(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
